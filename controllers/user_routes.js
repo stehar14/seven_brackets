bd972f227497418bb8100de9c1fb8e853e579934
afellman <red.kasid@gmail.com>
2018-02-22 21:16:50 -0500

    fixed users

--- controllers/user_routes.js
+++ controllers/user_routes.js
@@ -8,12 +8,10 @@ module.exports = function(app) {
 //  Add new user - Post / create
    app.post("api/newUser", function(req,res){
      db.User.create({
-      
       userName : req.body.userName,
       rating : null,
-      img : fbImg
-      
-     
+      img_url : req.body.img_url,
+      fbtoken: req.body.fbtoken
   }).then(function(userTable){
     res.json(userTable)
   
