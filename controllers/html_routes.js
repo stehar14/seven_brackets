50b1759c32eb1ca4792ee7eb2e049d1559f1fd64
afellman <red.kasid@gmail.com>
2018-02-23 08:50:37 -0500

    added redirect if not logged in

--- controllers/html_routes.js
+++ controllers/html_routes.js
@@ -2,4 +2,14 @@ module.exports = function(app, path) {
   app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/../views/home.html'));
   })
+
+  app.get('/profile', function(req, res) {
+    if(req.body.connected){
+      console.log(req.body.connected)
+    res.render('index')
+    } else {
+      res.redirect('/')
+    }
+  })
+
 }
