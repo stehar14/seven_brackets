8c01b1a49fa58044be9b9fd0c7258678db66f534
afellman <red.kasid@gmail.com>
2018-02-23 12:51:15 -0500

    fixed css paths. Moved home.html to public

--- controllers/html_routes.js
+++ controllers/html_routes.js
@@ -1,6 +1,6 @@
 module.exports = function(app, path) {
   app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/../views/home.html'));
+    res.sendFile(path.join(__dirname + '/../public/home.html'));
   })
 
   app.get('/profile', function(req, res) {
