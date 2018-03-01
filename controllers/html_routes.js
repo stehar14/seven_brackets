9f0b228adcaa35b49a09971a3c9af8c2a7795fea
Steve Harold <stehar14@yahoo.com>
2018-03-01 18:01:52 -0500

    forum html and css changes

--- controllers/html_routes.js
+++ controllers/html_routes.js
@@ -11,7 +11,7 @@ module.exports = function(app, path) {
     console.log('/profile get request received')
     // if(req.body.connected){
     //   console.log(req.body.connected)
-    res.render('forum', req.body)
+    res.render('profile', req.body)
     // } else {
     //   res.sendFile(path.join(__dirname + '/../public/waiting.html'));
     // }
