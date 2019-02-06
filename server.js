022dd9d3b7d0073922242f57269ea64dcdfee1bc
Steve H <stehar14@yahoo.com>
2019-02-05 21:18:40 -0500

    fixing conflicts

--- server.js
+++ server.js
@@ -4,11 +4,7 @@ var method = require('method-override');
 var path = require('path')
 
 // Set port for server = 8585
-<<<<<<< HEAD
-var PORT = 3000;
-=======
-var PORT = process.env.PORT || 3006;
->>>>>>> 0a1dca0f8cedc0bda3ba5337c31830539d4bd763
+var PORT =  3006;
 
 var app = express();
 
